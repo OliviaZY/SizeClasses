edab0f95c6e99133e5967e3660b3e1bdcc3ce811
CSSE Department <csse@cssemm-18.wlan.rose-hulman.edu>
2018-03-15 10:58:45 -0400

    Initial Commit

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by CSSE Department on 3/15/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
